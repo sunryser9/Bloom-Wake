ff32d4b47f05c07ad55788fbf6dbb4bacceecb7a
sunryser9 <42509762+sunryser9@users.noreply.github.com>
2026-08-20 10:37:33 +0530

    build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.bloomwake"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.bloomwake"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
     }
