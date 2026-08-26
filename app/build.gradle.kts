bf2ca3abfa46cef5417ddf6c8de835779a64635a
sunryser9 <42509762+sunryser9@users.noreply.github.com>
2026-08-26 15:05:51 +0530

    build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.bloomwake"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
     }
 
     signingConfigs {
